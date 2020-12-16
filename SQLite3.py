db74af6ab0d30967a8c4ce6e7498465aea080dcb
feel-exe <62546385+feel-exe@users.noreply.github.com>
2020-12-16 23:51:58 +0300

    пройдены функции работы с таблицами: добалвние строк, удаление и запрос данных.

--- SQLite3.py
+++ SQLite3.py
@@ -28,4 +28,53 @@ def sqlite3_example_create_bd():
 sqlite3_example_create_bd()
 
 
+def print_data_2d(columns_name, data):
+    print(columns_name)
+    for line in data:
+        print(line)
+    print('number of lines in database table is ' + str(len(data)))
+
+def sqlite3_simple_read_bd(data_base, table, column_name = None):
+    con = sqlite3.connect('Py_db.db')
+
+    cur = con.cursor()
+
+    query_columns = 'pragma table_info('+table+')'
+
+    cur.execute(query_columns)
+
+    columns_description = cur.fetchall()
+
+    columns_names = []
+
+    for columns in columns_description:
+        columns_names.append(columns[1])
+    print(columns_names)
+
+
+
+    if column_name is None:
+        query = 'SELECT * FROM '+table
+        cur.execute(query)
+        data = cur.fetchall()
+
+    else:
+        # чтение построчно из БД
+        query1 = 'SELECT'+column_name+' FROM ' + table
+        cur.execute(query1)
+        data = cur.fetchall()
+
+
+    print_data_2d(columns_names, data)
+
+    #cur.execute('SELECT Sample, Well, Porosity, Swr, Permeability FROM core_fes ORDER BY Well')
+    #data = cur.fetchall()
+    #cur.close()
+    #con.close()
+
+
+data_base = '/Py_db.db'
+table = 'core_fes'
+sqlite3_simple_read_bd(data_base, table, 'Sample')
+
 
